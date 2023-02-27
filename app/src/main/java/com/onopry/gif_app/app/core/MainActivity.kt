6cd26207336c29d2d07b2b262a4d0608164e9f56
Andro <Andromeda2516@gmail.com>
2023-02-28 01:39:36 +0700

    add basic project structure

--- app/src/main/java/com/onopry/gif_app/app/core/MainActivity.kt
+++ app/src/main/java/com/onopry/gif_app/app/core/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.onopry.gif_app
+package com.onopry.gif_app.app.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.onopry.gif_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
