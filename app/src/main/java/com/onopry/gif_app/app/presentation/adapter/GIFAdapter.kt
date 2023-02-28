b7042037a381dc67dda86b1c12eec21dc16d074b
Andro <Andromeda2516@gmail.com>
2023-02-28 17:59:53 +0700

    add: paging adapter, starting create viewModel for list of items

---
+++ app/src/main/java/com/onopry/gif_app/app/presentation/adapter/GIFAdapter.kt
@@ -0,0 +1,42 @@
+package com.onopry.gif_app.app.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.onopry.gif_app.R
+import com.onopry.gif_app.app.data.model.GifItem
+import com.onopry.gif_app.databinding.ItemGifBinding
+
+class GIFAdapter : PagingDataAdapter<GifItem, GIFAdapter.GIFViewHolder>(GIFDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GIFViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemGifBinding.inflate(layoutInflater, parent, false)
+        return GIFViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: GIFViewHolder, position: Int) {
+        holder.onBind(
+            getItem(position) ?: return
+        )
+    }
+
+
+    class GIFViewHolder(
+        private val binding: ItemGifBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(gif: GifItem) {
+            binding.gifTitleTv.text = gif.title
+            loadGif(gif)
+        }
+
+        private fun loadGif(gif: GifItem) =
+            Glide.with(binding.root)
+                .asGif()
+                .load(gif.url)
+                .placeholder(R.drawable.placeholder)
+                .into(binding.gifImg)
+    }
+}
--- app/src/main/java/com/onopry/gif_app/app/presentation/screen/list/ListViewModel.kt
+++ app/src/main/java/com/onopry/gif_app/app/presentation/screen/list/ListViewModel.kt
@@ -1,7 +1,22 @@
 package com.onopry.gif_app.app.presentation.screen.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.onopry.gif_app.app.data.model.GifItem
+import com.onopry.gif_app.app.data.repository.Repository
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+import java.util.concurrent.Flow
 
-class ListViewModel : ViewModel() {
+class ListViewModel(private val repo: Repository) : ViewModel() {
+
+    val gifFlow = flow<PagingData<GifItem>> {
+        repo.getPagedGIFs()
+    }.cachedIn(viewModelScope)
+
 
 }
